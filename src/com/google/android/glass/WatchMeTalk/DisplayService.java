2ad68f0556ff3cc0fa298f9ae97f8c8b4133bfd1
gueroso <cjguerin93@gmail.com>
2014-01-25 12:59:43 -0800

    initial commit for revised edition

---
+++ src/com/google/android/glass/WatchMeTalk/DisplayService.java
@@ -0,0 +1,94 @@
+package com.google.android.glass.WatchMeTalk;
+
+import java.util.ArrayList;
+
+import com.google.android.glass.timeline.LiveCard;
+import com.google.android.glass.timeline.TimelineManager;
+
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.widget.RemoteViews;
+
+public class DisplayService extends Service {
+	
+	private TimelineManager mTimeLineManager;
+	private boolean update;
+	private LiveCard mLiveCard;
+	private String cardID = "WatchMeTalk";
+	private String cardText="";
+	
+	
+	
+	ArrayList<String> list = Util.ArrayList;
+	
+	
+	
+	
+	@Override
+	public void onCreate(){
+		super.onCreate();
+		mTimeLineManager = TimelineManager.from(this);
+	}
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId){
+		update = intent.getBooleanExtra("update", false);
+		
+		if(mLiveCard == null){
+			mLiveCard = mTimeLineManager.createLiveCard(cardID);
+			RemoteViews remoteViews = new RemoteViews(this.getPackageName(), R.layout.card_layout);
+			if (list.size() > 0){
+				for (int i = 0; i< list.size() && i < 5; i ++){
+					cardText += "" + list.get(i) + "\n";
+				}
+				remoteViews.setTextViewText(R.id.memo_list_text_view, cardText);
+				remoteViews.setTextViewText(R.id.no_memos_text_view, "");
+			}
+			else{
+				remoteViews.setTextViewText(R.id.memo_list_text_view, "");
+				remoteViews.setTextViewText(R.id.no_memos_text_view, "No One Spoke");
+			}
+			mLiveCard.setViews(remoteViews);
+			Intent menu;
+			if (list.size() == 0){
+				menu = new Intent(this, ViewMemoMenuActivityNoMemo.class);
+			}
+			else{
+				menu = new Intent(this, ViewMemoMenuActivity.class);
+			}
+			mLiveCard.setAction(PendingIntent.getActivity(this, 0, menu, 0));
+			
+			if (update){
+				mLiveCard.publish(LiveCard.PublishMode.SILENT);
+			}
+			else{
+				mLiveCard.publish(LiveCard.PublishMode.SILENT);
+			}
+		}
+		else{
+			mLiveCard.unpublish();
+			mLiveCard.publish(LiveCard.PublishMode.REVEAL);
+		
+		}
+		return START_STICKY;
+		
+	}
+	
+	@Override
+	public void onDestroy(){
+		if (mLiveCard!=null && mLiveCard.isPublished())
+		{
+			mLiveCard.unpublish();
+			mLiveCard = null;
+		}
+		super.onDestroy();
+	}
+	
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
---
+++ src/com/google/android/glass/WatchMeTalk/MainActivity.java
@@ -0,0 +1,68 @@
+package com.google.android.glass.WatchMeTalk;
+
+import java.util.ArrayList;
+
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+
+
+public class MainActivity extends Activity {
+
+	public static final int SPEECH_REQUEST = 0;
+	private String memoResult;
+	private ArrayList<String> memoList;
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		displaySpeechRecognizer();
+	}
+	
+	private void displaySpeechRecognizer()
+	{
+		Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+		startActivityForResult(intent, SPEECH_REQUEST);
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data)
+	{
+		if(requestCode == SPEECH_REQUEST && resultCode == RESULT_OK)
+		{
+			//Get results of speech to text
+			memoResult = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS).get(0);
+			//get list of memos saved in shared prefs
+			if (Utils.checkForObjectInSharedPrefs(this,getString(R.string.shared_memo_key)))
+			{
+				memoList = new ArrayList<String>(Utils.getStringArrayPref(this, getString(R.string.shared_memo_key)));
+			}
+			else{
+				memoList = new ArrayList<String>();
+			}
+			//add the new memo to the list
+			memoList.add(memoResult);
+			//Save the new list
+			ArrayList<String> memoArrayList = new ArrayList<String>(memoList);
+			
+			//update the list in the live card
+			stopService(new Intent(this, ViewMemoService.class));
+			Intent serviceIntent = new Intent(this, ViewMemoService.class);
+			serviceIntent.putExtra("update", true);
+			startService(serviceIntent);
+		}
+		super.onActivityResult(requestCode, resultCode, data);
+		finish();
+	}
+
+	@Override
+	public void onResume()
+	{
+		super.onResume();
+	}
+
+}
